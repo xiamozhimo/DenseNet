1ab3a0fd62b11302dde10214c4ef1bf830bab2e5
Tony Fu <tfu@isb.bj.edu.cn>
2019-06-27 08:22:17 +0800

    06272019

--- awesome-python3-webapp/src/www/app.py
+++ awesome-python3-webapp/src/www/app.py
@@ -1,24 +1,25 @@
-'''
-Created on Jun 26, 2019
 
-@author: tfu
-'''
 import logging; logging.basicConfig(level=logging.INFO)
-
 import asyncio, os, json, time
 from datetime import datetime
-
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+    return web.Response(body = b'<h1>Hi, there!</h1>', headers = {"content-type": "text/html"})
+
+def login(request):
+    text = '<h1>Login %s!</h1>' % request.match_info['name']
+    return web.Response(body = text.encode(), headers = {"content-type": "text/html"})
 
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+    app = web.Application()
+    app.add_routes([web.get("/", index),web.get("/login{name}", login)])
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '127.0.0.1', 8000)
     logging.info('server started at http://127.0.0.1:8000...')
-    return srv
+    await site.start()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
