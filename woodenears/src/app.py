adb086d2c45be796ff6abdc4d1e5ad37d35d6d05
Tony Fu <tfu@isb.bj.edu.cn>
2019-07-30 13:18:54 +0800

    07302019

--- woodenears/src/app.py
+++ woodenears/src/app.py
@@ -8,21 +8,48 @@ from bs4 import BeautifulSoup
 from werkzeug import urls
 import os
 
-url='https://www.woodenears.com/list?category=5b6171cf823aea488f19e1d0&name=%E8%80%B3%E6%9C%BA%E6%B5%8B%E9%87%8F%E6%8A%A5%E5%91%8A&pageIndex=1'
-jumproot='https://www.woodenears.com/article/'
+url='https://www.woodenears.com/list?category=5b6171cf823aea488f19e1d0&name=%E8%80%B3%E6%9C%BA%E6%B5%8B%E9%87%8F%E6%8A%A5%E5%91%8A'
+baseJumpRoot='https://www.woodenears.com'
 
-def getHtmlUrl(url):
+
+def getPageRange(url):    
+    pageRange=[]    
     r= requests.get(url)
     r.raise_for_status()
     r.encoding= r.apparent_encoding
-    return r.text
-
-def getjumplink(jumproot,html):
+    html=r.text
     soup=BeautifulSoup(html,"html.parser")
-    boxes=soup.find('div',id="listContent").find_all('a')
-    for box in boxes:
-        print(box['href'])
+    numbers = soup.find('ul',class_="el-pager").find_all('li')
+    for number in numbers:
+        pageRange.append(number.text)
+    return pageRange
 
     
-html=getHtmlUrl(url)
-getjumplink(jumproot,html)
+    
+
+def getHtmlUrl(url,pageRange):
+    indexList=[]
+    for i in pageRange:
+        urljump=url+'&pageIndex='+i
+        r= requests.get(urljump)
+        r.raise_for_status()
+        r.encoding= r.apparent_encoding
+        indexList.append(r.text)
+    return indexList
+
+def getjumplink(baseJumpRoot,indexList):
+    pageList=[]
+    for html in indexList:
+        soup=BeautifulSoup(html,"html.parser")
+        links=soup.find('div',id="listContent").find_all('a')
+        for link in links:
+            pageList.append(baseJumpRoot+link['href'])
+    return pageList
+
+
+
+pageRange=getPageRange(url)
+indexList=getHtmlUrl(url,pageRange)
+pageList = getjumplink(baseJumpRoot,indexList)
+print(pageList)
+
