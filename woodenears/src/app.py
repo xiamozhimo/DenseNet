15b95edd23cf87689605c92dc0f28f68df88bfce
Tony Fu <tfu@isb.bj.edu.cn>
2019-07-30 13:52:59 +0800

    07302019

--- woodenears/src/app.py
+++ woodenears/src/app.py
@@ -11,30 +11,26 @@ import os
 url='https://www.woodenears.com/list?category=5b6171cf823aea488f19e1d0&name=%E8%80%B3%E6%9C%BA%E6%B5%8B%E9%87%8F%E6%8A%A5%E5%91%8A'
 baseJumpRoot='https://www.woodenears.com'
 
-
-def getPageRange(url):    
-    pageRange=[]    
+def getHtml(url):
     r= requests.get(url)
     r.raise_for_status()
     r.encoding= r.apparent_encoding
-    html=r.text
+    return r.text
+
+def getPageRange(url):    
+    pageRange=[]    
+    html=getHtml(url)
     soup=BeautifulSoup(html,"html.parser")
     numbers = soup.find('ul',class_="el-pager").find_all('li')
     for number in numbers:
         pageRange.append(number.text)
     return pageRange
 
-    
-    
-
 def getHtmlUrl(url,pageRange):
     indexList=[]
     for i in pageRange:
         urljump=url+'&pageIndex='+i
-        r= requests.get(urljump)
-        r.raise_for_status()
-        r.encoding= r.apparent_encoding
-        indexList.append(r.text)
+        indexList.append(getHtml(urljump))
     return indexList
 
 def getjumplink(baseJumpRoot,indexList):
@@ -48,8 +44,37 @@ def getjumplink(baseJumpRoot,indexList):
 
 
 
+def savePics(pageList):
+    for pagelink in pageList:
+        html=getHtml(pagelink)
+        soup=BeautifulSoup(html,"html.parser")
+        all_img=soup.find('div',class_='content-container').find_all('img')    
+        for img in all_img:
+            src=img['src']
+            print(src)
+            root='D:/wormgetpic/'
+            path=root + src.split('/')[-1]+'.jpg'
+            try:
+                if not os.path.exists(root):
+                    os.mkdir(root)
+                if not os.path.exists(path):
+                    r= requests.get(src)
+                    with open(path,'wb') as f:
+                        f.write(r.content)
+                    print('Success')   
+                else:
+                    print('Already Exist')       
+            except Exception as e:
+                print(e)
+            
+pagelist=['https://www.woodenears.com/article/5bed14bbbcd7ab780aff6e0e']
+savePics(pagelist)
+
+'''
 pageRange=getPageRange(url)
 indexList=getHtmlUrl(url,pageRange)
 pageList = getjumplink(baseJumpRoot,indexList)
 print(pageList)
+'''   
+
 
