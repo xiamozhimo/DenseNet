b417a30d7c0382f7dcc952349a9ef18de8f11999
Tony Fu <tfu@isb.bj.edu.cn>
2019-07-31 14:01:40 +0800

    Demo Version

--- woodenears/src/app.py
+++ woodenears/src/app.py
@@ -9,32 +9,21 @@ from bs4 import BeautifulSoup
 from werkzeug import urls
 import os
 import re
-from indextool.indexhtml import getHtml,getHtml,getHtmlUrl,getjumplink
-from subpagetool.subpagehtml import getReportName,savePics
+from indextool.indexhtml import getHtml,getHtml,getHtmlUrl,getjumplink,getPageRange
+from subpagetool.subpagehtml import getReportName
+from filesystem.folderManage import makeFolderPath,makeFilePath,checkmakefolder,savePics
 
 url='https://www.woodenears.com/list?category=5b6171cf823aea488f19e1d0&name=%E8%80%B3%E6%9C%BA%E6%B5%8B%E9%87%8F%E6%8A%A5%E5%91%8A'
 baseJumpRoot='https://www.woodenears.com'
+root='D:/wormgetpic/'
 
+def main():
+    indexlist=getHtmlUrl(url)
+    pageList=getjumplink(baseJumpRoot,indexlist)
+    savePics(pageList,root)
 
-
-
-
+main()
                 
-'''                   
-html=getHtml('https://www.woodenears.com/article/5bed14bbbcd7ab780aff6e0e')                
-name=getReportName(html)
-print(name)
-
-pageRange=getPageRange(url)
-indexList=getHtmlUrl(url,pageRange)
-pageList = getjumplink(baseJumpRoot,indexList)
-print(pageList)                
-                
-                
-                
-'''            
-pagelist=['https://www.woodenears.com/article/5bed14bbbcd7ab780aff6e0e']
-savePics(pagelist)
 
 
 
