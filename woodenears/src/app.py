32993ab370aafb052b56f23308f682cd48718487
Tony Fu <tfu@isb.bj.edu.cn>
2019-09-23 09:24:37 +0800

    09232019

--- woodenears/src/app.py
+++ woodenears/src/app.py
@@ -4,14 +4,9 @@ Created on Jul 29, 2019
 @author: tfu
 '''
 
-import requests
-from bs4 import BeautifulSoup
-from werkzeug import urls
-import os
-import re
-from indextool.indexhtml import getHtml,getHtml,getHtmlUrl,getjumplink,getPageRange
-from subpagetool.subpagehtml import getReportName
-from filesystem.folderManage import makeFolderPath,makeFilePath,checkmakefolder,savePics
+from indextool.indexhtml import getHtmlUrl,getjumplink
+from filesystem.folderManage import savePics
+
 
 url='https://www.woodenears.com/list?category=5b6171cf823aea488f19e1d0&name=%E8%80%B3%E6%9C%BA%E6%B5%8B%E9%87%8F%E6%8A%A5%E5%91%8A'
 baseJumpRoot='https://www.woodenears.com'
